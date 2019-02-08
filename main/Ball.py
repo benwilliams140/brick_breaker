c727796f994e7f7cc85fb9963e12047afcdadaee
Ben Williams <ben.williams14@outlook.com>
2019-02-08 00:24:48 -0500

    Update Ball.py
    Moved rendering of the Ball to it's own function.  Added a function to reset the variables to their original state.  Added directional changes in the x-direction when colliding with the paddle.

--- main/Ball.py
+++ main/Ball.py
@@ -1,3 +1,5 @@
+import pygame
+
 class Ball:
 	def __init__(self, paddle, x, y, radius, velocityX, velocityY, screenWidth, screenHeight):
 		self.x = x
@@ -10,7 +12,17 @@ class Ball:
 		self.running = True
 		self.paddle = paddle
 		
-	def updatePosition(self):
+	def render(self, screen):
+		pygame.draw.circle(screen, (255, 255, 255), (int(self.x), int(self.y)), self.radius)
+		
+	def reset(self, x, y, velocityX, velocityY):
+		self.x = x
+		self.y = y
+		self.velocityX = velocityX
+		self.velocityY = velocityY
+		self.running = True
+		
+	def update(self):
 		self.x += self.velocityX
 		self.y += self.velocityY
 		
@@ -19,10 +31,20 @@ class Ball:
 		if self.paddle.getX() < self.x < self.paddle.getX() + self.paddle.getWidth():
 			if self.y + self.radius > self.paddle.getY():
 				self.velocityY *= -1
+				if self.x - self.paddle.getX() < 50:
+					self.velocityX = -int(100 / (self.x - self.paddle.getX()))
+				elif self.x - self.paddle.getX() > 150:
+					self.velocityX = int((self.x - self.paddle.getX()) / 20)
 		if self.y + self.radius > self.screenHeight:
 			self.running = False
 		if self.y - self.radius < 0:
 			self.velocityY *= -1
+			
+	def flipDirectionX(self):
+		self.velocityX *= -1
+		
+	def flipDirectionY(self):
+		self.velocityY *= -1
 		
 	def getX(self):
 		return self.x
