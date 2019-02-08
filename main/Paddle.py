7f566692090a7ff7a6189bd6241a63eedf70278c
Ben Williams <ben.williams14@outlook.com>
2019-02-08 00:23:16 -0500

    Update Paddle.py
    Moved rendering code of Paddle to it's own function.  Added a function to reset the Paddle upon game over.

--- main/Paddle.py
+++ main/Paddle.py
@@ -1,47 +1,55 @@
+import pygame
+
 class Paddle:
-    def __init__(self, x, y, width, height, velocity, screenWidth):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.velocity = velocity
-        self.colour = (255, 255, 255)
-        self.direction = "null"
-        self.screenWidth = screenWidth
+	def __init__(self, x, y, width, height, velocity, screenWidth):
+		self.x = x
+		self.y = y
+		self.width = width
+		self.height = height
+		self.velocity = velocity
+		self.colour = (255, 255, 255)
+		self.direction = "null"
+		self.screenWidth = screenWidth
 		
-    def updatePosition(self):
-        if self.direction == "left":
-            self.x -= self.velocity
-        elif self.direction == "right":
-            self.x += self.velocity
+	def reset(self, x):
+		self.x = x
+		
+	def render(self, screen):
+		pygame.draw.rect(screen, (255, 255, 255), (self.x, self.y, self.width, self.height))
+		
+	def update(self):
+		if self.direction == "left":
+			self.x -= self.velocity
+		elif self.direction == "right":
+			self.x += self.velocity
 			
-        if self.x < 0: self.x = 0
-        if self.x + self.width > self.screenWidth: self.x = self.screenWidth - self.width
+		if self.x < 0: self.x = 0
+		if self.x + self.width > self.screenWidth: self.x = self.screenWidth - self.width
 
-    def setPosition(self, x, y):
-        self.x = x
-        self.y = y
+	def setPosition(self, x, y):
+		self.x = x
+		self.y = y
 
-    def setDirection(self, direction):
-        self.direction = direction
+	def setDirection(self, direction):
+		self.direction = direction
 
-    def getX(self):
-        return self.x
+	def getX(self):
+		return self.x
 
-    def getY(self):
-        return self.y
+	def getY(self):
+		return self.y
 
-    def getWidth(self):
-        return self.width
+	def getWidth(self):
+		return self.width
 
-    def getHeight(self):
-        return self.height
+	def getHeight(self):
+		return self.height
 
-    def getVelocity(self):
-        return self.velocity
+	def getVelocity(self):
+		return self.velocity
 
-    def getColour(self):
-        return self.colour
+	def getColour(self):
+		return self.colour
 
-    def getDirection(self):
-        return self.direction
+	def getDirection(self):
+		return self.direction
